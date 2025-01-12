90545d548a557262f12855de62eeff585b79d971
wanning-lu <wanning.lu@gmail.com>
2025-01-12 15:46:39 -0800

    changed motor controls to play/pause

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { useEffect, useState, useRef } from 'react';
 import './App.scss';
 import {io} from "socket.io-client";
-import { Paper, Slider, TextField, Typography, Stack, Switch, Button } from '@mui/material'
+import { Paper, Slider, TextField, SvgIcon, IconButton, Typography, Stack, Switch, Button } from '@mui/material'
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid2'
 import ThreeD from './3D';
 import toast, {Toaster} from "react-hot-toast";
+
+import Pause from '@mui/icons-material/Pause';
+import RotateRight from '@mui/icons-material/RotateRight';
+import RotateLeft from '@mui/icons-material/RotateLeft';
 
 const SOCKET_URL = 'https://rangersw.com';
 
@@ -14,13 +18,15 @@ function App() {
 	const connecting = useRef(false);
 	const socket_connection = useRef(null);
 
-	// stores slider values
-	const [sliderInput, handleSliderUpdate] = useState({
-        angle1: 0,
-        angle2: 0,
-        angle3: 0,
-        angle3Input: "0",
+	// stores motor speed values
+	const [motorInput, handleSpeedUpdate] = useState({
+        imageMotor: 0,
+		filterMotor: 0
     });
+
+	// stores state of 2x button
+	const [isFilterTwoTimes, handleFilterTwoTimes] = useState(false)
+	const [isImageTwoTimes, handleImageTwoTimes] = useState(false)
 
 	// stores current dimension state (2D or 3D)
 	const [is3D, switchDimensions] = useState(false)
@@ -61,7 +67,7 @@ function App() {
             // there is a new gear state, update the gear
             socket.on('gear', (data) => {
                 // set_gear_state(data);
-				handleSliderUpdate({angle1 : data.gear1, angle2 : data.gear2, angle3 : data.gear3, angle3Input : data.gear3.toString()})
+				handleSpeedUpdate({imageMotor : 0, filterMotor : 0})
                 toast.success('Gear State has been retrieved or updated.',  {
                     duration: 5000
                 });
@@ -97,24 +103,46 @@ function App() {
 					</div>
 					
 					<div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-						<Typography textAlign='left' style={{ marginLeft: 3 }} >Turbo encabulator</Typography>
+						
 						<div style={{ display: 'flex', flexDirection: 'row', marginTop: 20, width: '100%' }}>
-							<Slider
-								valueLabelDisplay="on"
-								value={sliderInput.angle1}
-								onChange={(event, value) => {handleSliderUpdate({...sliderInput, angle1: value })}}
-								onChangeCommitted={(event, value) => {
-									socket_connection.current.emit('adjust', {
-										gear: 1,
-										value: value
-									});
-								}}
-								min={-90}
-								max={90}
-							/>
+							<Typography textAlign='left' style={{ marginLeft: 3 }} >Filter motor</Typography>
+							<IconButton onClick={() => {handleSpeedUpdate({...motorInput, filterMotor: -1 * (isFilterTwoTimes ? 2 : 1)}); console.log(motorInput)}}>
+								<RotateLeft />
+							</IconButton>
+							<IconButton onClick={() => {handleSpeedUpdate({...motorInput, filterMotor: 0}); console.log(motorInput)}}>
+								<Pause />
+							</IconButton>
+							<IconButton onClick={() => {handleSpeedUpdate({...motorInput, filterMotor: (isFilterTwoTimes ? 2 : 1)}); console.log(motorInput)}}>
+								<RotateRight />
+							</IconButton>
+							<IconButton onClick={() => {handleSpeedUpdate({...motorInput, filterMotor: motorInput.filterMotor * (isFilterTwoTimes ? 0.5 : 2)}); handleFilterTwoTimes(!isFilterTwoTimes); console.log(motorInput)}}>
+								<SvgIcon>
+								<svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 449.851"><g fill-rule="nonzero"><path fill={isFilterTwoTimes ? "blue" : "black"} d="M153.143 219.364l-4.867-26.516c15.716-4.737 31.372-7.106 46.96-7.106 6.241 0 11.445.183 15.624.559 4.178.376 8.513 1.31 13.003 2.809 4.49 1.493 8.046 3.55 10.667 6.171 5.988 5.988 8.98 15.157 8.98 27.505 0 12.348-3.491 21.452-10.479 27.317-6.982 5.86-21.704 12.595-44.154 20.206v7.955h52.763v27.219h-95.984v-23.385c0-15.525 3.97-23.486 15.715-33.117 3.367-2.87 7.89-5.614 13.567-8.234a4349.194 4349.194 0 0116.091-7.39c5.05-2.31 9.137-4.211 12.253-5.706v-11.468c-5.613-.627-10.787-.938-15.528-.938-11.474 0-23.016 1.374-34.611 4.119zm144.908-30.818l12.908 31.248h1.873l12.908-31.248h40.599l-26.006 56.503 26.006 60.434h-41.534l-14.034-33.676h-1.681l-13.848 33.676h-39.664l25.447-59.311-25.447-57.626h42.473z"/></g></svg>
+								</SvgIcon>
+							</IconButton>
+							
 						</div>
 
-						<Typography textAlign='left' style={{ marginLeft: 3 }} >Prefabulated alumnite</Typography>
+						<div style={{ display: 'flex', flexDirection: 'row', marginTop: 20, width: '100%' }}>
+							<Typography textAlign='left' style={{ marginLeft: 3 }} >Image motor</Typography>
+							<Typography textAlign='left' style={{ marginLeft: 3 }} >Filter motor</Typography>
+							<IconButton onClick={() => {handleSpeedUpdate({...motorInput, imageMotor: -1 * (isImageTwoTimes ? 2 : 1)}); console.log(motorInput)}}>
+								<RotateLeft />
+							</IconButton>
+							<IconButton onClick={() => {handleSpeedUpdate({...motorInput, imageMotor: 0}); console.log(motorInput)}}>
+								<Pause />
+							</IconButton>
+							<IconButton onClick={() => {handleSpeedUpdate({...motorInput, imageMotor: (isImageTwoTimes ? 2 : 1)}); console.log(motorInput)}}>
+								<RotateRight />
+							</IconButton>
+							<IconButton onClick={() => {handleSpeedUpdate({...motorInput, imageMotor: motorInput.imageMotor * (isImageTwoTimes ? 0.5 : 2)}); handleImageTwoTimes(!isImageTwoTimes); console.log(motorInput)}}>
+								<SvgIcon>
+								<svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 449.851"><g fill-rule="nonzero"><path fill={isImageTwoTimes ? "blue" : "black"} d="M153.143 219.364l-4.867-26.516c15.716-4.737 31.372-7.106 46.96-7.106 6.241 0 11.445.183 15.624.559 4.178.376 8.513 1.31 13.003 2.809 4.49 1.493 8.046 3.55 10.667 6.171 5.988 5.988 8.98 15.157 8.98 27.505 0 12.348-3.491 21.452-10.479 27.317-6.982 5.86-21.704 12.595-44.154 20.206v7.955h52.763v27.219h-95.984v-23.385c0-15.525 3.97-23.486 15.715-33.117 3.367-2.87 7.89-5.614 13.567-8.234a4349.194 4349.194 0 0116.091-7.39c5.05-2.31 9.137-4.211 12.253-5.706v-11.468c-5.613-.627-10.787-.938-15.528-.938-11.474 0-23.016 1.374-34.611 4.119zm144.908-30.818l12.908 31.248h1.873l12.908-31.248h40.599l-26.006 56.503 26.006 60.434h-41.534l-14.034-33.676h-1.681l-13.848 33.676h-39.664l25.447-59.311-25.447-57.626h42.473z"/></g></svg>
+								</SvgIcon>
+							</IconButton>
+						</div>
+
+						{/* <Typography textAlign='left' style={{ marginLeft: 3 }} >Prefabulated alumnite</Typography>
 						<div style={{ display: 'flex', flexDirection: 'row', marginTop: 10, width: '80%' }}>
 							<Slider
 								value={sliderInput.angle2}
@@ -160,7 +188,7 @@ function App() {
 								sx={{ width: 80 }}
 								inputProps={{ style: { textAlign: 'right' } }}
 							/>
-						</div>
+						</div> */}
 					</div>
 				</Grid>
 

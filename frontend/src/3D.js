57775e75f466ed3f400cb2ab2d9c0bf6729fde4a
harry-lons <harrylonsdale25@gmail.com>
2025-04-07 00:25:02 -0700

    removed unnecessary CAD files

--- frontend/src/3D.js
+++ frontend/src/3D.js
@@ -22,60 +22,6 @@ function ThreeD() {
             </RaycastWrapper>
         </div>
     );
-
-    // const [selectedModel, selectModelType] = useState("None")
-
-    // const selectModel = (model) => {
-    //     selectModelType(model);
-    // };
-
-    // const handleChange = (event) => {
-    //     const key = event.target.value;
-    //     selectModelType(key);
-    // };
-
-    // const modelMap = {
-    //     'None': <div />,
-    //     // 'boxes': <BoxModel />,
-    //     // 'ThorLabs': <ThorLabs />,
-    //     // 'PlanarGrid': <PlanarGrid />,
-    //     // 'LabeledMulti1': <MultiModel />,
-    //     // 'LabeledMulti2': <MultiModel2 />,
-    //     // 'FourierOpticsTest': <FourierOpticsTest />,
-    //     // 'FourierOptics': <FourierOptics />,
-    //     'ClickableFourierOptics': <ClickableFourierOptics />,
-    //     'RayCasting': <RaycastWrapper><RaycastModel /></RaycastWrapper>
-    // }
-
-    // return (
-    //     <>
-    //         {/* <div className="intro-card"> */}
-    //         <div>
-    //             <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
-    //                 <p>Welcome to LabStream 3D. Select a model to view.</p>
-    //                 <FormControl color="secondary" variant="outlined" sx={{ minWidth: 'auto', width: 'auto' }}>
-    //                     {/* <InputLabel sx={{color: 'red', backgroundColor: 'black', textDecorationColor: 'black', textEmphasisColor: 'black', }}>Select a Model</InputLabel> */}
-    //                     <Select
-    //                         value={selectedModel}
-    //                         onChange={handleChange}
-    //                         // label="Select a Model"
-    //                         sx={{ minWidth: 120, maxWidth: 200 }}
-    //                     >
-    //                         {Object.entries(modelMap).map(([key]) => (
-    //                             <MenuItem key={key} value={key}>{key}</MenuItem>
-    //                         ))}
-    //                     </Select>
-    //                 </FormControl>
-    //             </div>
-    //         </div>
-    //         <div style={{ marginTop: 40 }}>
-    //             {
-    //                 // Display the selected model
-    //                 modelMap[selectedModel]
-    //             }
-    //         </div>
-    //     </>
-    // );
 }
 
 export default ThreeD

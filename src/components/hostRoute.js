34d99abf2e829cffc1acfccf9af1742503590493
Ridhi <rsrikanth@ucsd.edu>
2025-07-04 10:32:52 -0500

    host updated

--- src/components/hostRoute.js
+++ src/components/hostRoute.js
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from "react-router-dom";
 import { useUser } from './UserContext';
 
 const HostRoute = () => {
-    const hosts = ["rsrikanth@ucsd.edu", "wal009@ucsd.edu", "sabaghda@ucsd.edu", "hlonsdale@ucsd.edu", "prashk135@gmail.com"];
+    const hosts = ["rsrikanth@ucsd.edu", "wal009@ucsd.edu", "sabaghda@ucsd.edu", "hlonsdale@ucsd.edu", "prashk135@gmail.com", "yij036@ucsd.edu"];
     const { user } = useUser();
     console.log(user.email);
     if (hosts.includes(user.email)) {
